1958af8ce93ab12bbe6c6cc786190ca36cf3a826
jalalmourad <114366973+jalalmourad@users.noreply.github.com>
2022-10-20 22:17:45 -0400

    Add files via upload

---
+++ Hand.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Hand{
+
+    ArrayList<Piece> hand;
+
+    public Hand(){
+
+        hand = new ArrayList<Piece>(7);
+
+    }
+
+    public void addPiece(Piece piece){
+        hand.add(piece);
+    }
+
+    public void removePiece(Piece piece){
+        hand.remove(piece);
+    }
+
+    public void printHand(){
+        for (Piece p : hand){
+            System.out.println(p);
+        }
+    }
+
+
+
+    public static void main(String args[]) {
+        Hand h = new Hand();
+        Piece p = new Piece("U");
+        Piece p1 = new Piece("Q");
+        Piece p2= new Piece("B");
+        Piece p3= new Piece("N");
+
+        h.addPiece(p);
+        h.addPiece(p1);
+        h.addPiece(p2);
+        h.addPiece(p3);
+        h.addPiece(p);
+
+        h.printHand();
+
+
+    }
+
+}

bdbc4a8bb09a1e34189445a66baad2b85cf4bafd
Triguy12 <tritoncrowley@gmail.com>
2022-10-21 18:09:43 -0400

    Triton - added bag and game

---
+++ src/Bag.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Bag {
+    ArrayList<Piece> letterBag;
+
+    public Bag(char[] letters, int[] numOfEachLetter){
+        letterBag = new ArrayList<Piece>();
+        ArrayList<Piece> uniquePieces = new ArrayList<Piece>();
+        int i = 0;
+        for(char c : letters){  // makes each letter in the alphabet into a piece and add it to an arrayList
+            uniquePieces.add(new Piece(c));
+            i++;
+        }
+        i = 0;
+        for (Piece p : uniquePieces) {  // adds the appropriate amount of each letter as a piece to the letter bag
+            int numToAdd = numOfEachLetter[i];
+            while(numToAdd > 0){
+                letterBag.add(uniquePieces.get(i));
+                numToAdd--;
+            }
+            i++;
+        }
+        // Collections.shuffle(letterBag); the grab function pulls from a random index so randomizing the bag can be optional
+    }
+    public int remainingLetters(){
+        return letterBag.size();
+    }
+
+    public Piece grabPiece(){  //returns a random piece and removes that piece from this bag
+        Random random = new Random();
+        int randIndex = random.nextInt(remainingLetters());
+        Piece toReturn = letterBag.get(randIndex);
+        letterBag.remove(randIndex);
+        return toReturn;
+    }
+    public ArrayList<Piece> grabPiece(int number){  //returns a random piece and removes that piece from this bag
+        Random random = new Random();
+        ArrayList<Piece> pieces = new ArrayList<Piece>();  // need to limit number upto 7
+        for (int i =0; i < number; i++){
+            int randIndex = random.nextInt(remainingLetters());
+            Piece toReturn = letterBag.get(randIndex);
+            pieces.add(toReturn);
+            letterBag.remove(randIndex);
+        }
+        return pieces;
+    }
+
+    public String toString(){
+        ArrayList<Piece> letters = letterBag;
+        String bagString = "";
+        Piece lastPiece = null;
+        int numOf = 0;
+        for (Piece p : letters) {
+            if(lastPiece == null){
+                numOf++;
+                lastPiece = p;
+                continue;
+            }
+            if(lastPiece.letter == p.letter){
+                numOf++;
+            }
+            else{
+                bagString += lastPiece.letter + ": " + numOf + "\n";
+                numOf = 1;
+            }
+            lastPiece = p;
+        }
+        return bagString;
+    }
+/*
+    public ArrayList<Piece> Swap(ArrayList<Piece> p){
+
+    }*/
+}
---
+++ src/Game.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Game {
+    final static char[] ALPHABET = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    final static int[] POINTS = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    final static int[] NUMBER_OF_EACH = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1};
+
+    // need to create a bag of scrabble letters with appropriate pieces
+
+    /*
+    2 blank tiles (scoring 0 points)
+    1 point: E ×12, A ×9, I ×9, O ×8, N ×6, R ×6, T ×6, L ×4, S ×4, U ×4
+    2 points: D ×4, G ×3
+    3 points: B ×2, C ×2, M ×2, P ×2
+    4 points: F ×2, H ×2, V ×2, W ×2, Y ×2
+    5 points: K ×1
+    8 points: J ×1, X ×1
+    10 points: Q ×1, Z ×1
+    */
+
+    public static void main(String args[]){
+        Bag letters = new Bag(ALPHABET, NUMBER_OF_EACH);
+
+        /*
+        for (Piece p : letters.letterBag) {
+            System.out.println(p.letter);
+        }
+        */
+
+        /*
+        Piece p = letters.grabPiece();
+
+        ArrayList<Piece> ps = letters.grabPiece(4);
+
+        System.out.println(p.letter);
+
+        for (Piece piece : ps) {
+            System.out.print(piece.letter);
+        }
+        */
+        ArrayList<Piece> ps = letters.grabPiece(50);
+        String out = letters.toString();
+        System.out.println(out);
+
+        /*
+        for (Piece piece : ps) {
+            System.out.print(piece.letter);
+        }*/
+
+    }
+
+}

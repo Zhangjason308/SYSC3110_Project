9ad1ca4b9a4cd83206fb6c4d3ada19cf2524cf77
Zhangjason308 <zhangjason308@gmail.com>
2022-10-24 15:20:19 -0400

    change String elements to Pieces

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,5 @@
 public class Board {
-        String[][] arr;
+        Piece[][] arr;
         class placedPiece{
             static int x;
             static int y;
@@ -14,14 +14,14 @@ public class Board {
         }
 
     public Board(){
-        arr = new String[15][15]; //Change to Element After, and once changed to type PIECE, every element will be null
+        arr = new Piece[15][15]; //Change to Element After, and once changed to type PIECE, every element will be null
     }
 
-    public String getPiece(int x, int y) {
+    public Piece getPiece(int x, int y) {
         return arr[x][y];
     }
 
-    public void placePiece(int x, int y, String E) {
+    public void placePiece(int x, int y, Piece E) {
         arr[x][y] = E;
         placedPiece.x = x;
         placedPiece.y = y;
@@ -38,7 +38,7 @@ public class Board {
         }
         firstLetter.y--;
         while(arr[firstLetter.x][firstLetter.y] != null) {
-            word.y+=arr[firstLetter.x][firstLetter.y];
+            word.y+=arr[firstLetter.x][firstLetter.y].getLetter();
             firstLetter.y--;
         }
         word.y.toLowerCase();
@@ -51,7 +51,7 @@ public class Board {
         }
         firstLetter.x++;
         while(arr[firstLetter.x][firstLetter.y] != null) {
-            word.x +=arr[firstLetter.x][firstLetter.y];
+            word.x +=arr[firstLetter.x][firstLetter.y].getLetter();
             firstLetter.x++;
         }
         word.x.toLowerCase();
@@ -67,14 +67,14 @@ public class Board {
         return 1;
     }
 
-     public void newMethod() {
-
-     }
     public static void main(String args[]) {
         Board board1 = new Board();
-        board1.placePiece(1,1,"T");
-        board1.placePiece(2,1,"H");
-        board1.placePiece(3,1,"E");
+        Piece t = new Piece("T");
+        Piece h = new Piece("H");
+        Piece e = new Piece("E");
+        board1.placePiece(1,1,t);
+        board1.placePiece(2,1,h);
+        board1.placePiece(3,1,e);
         board1.getWords();
         System.out.println("Horizontal Word: " + word.x);
         System.out.println("Vertical Word: " + word.y);
